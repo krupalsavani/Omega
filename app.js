ea4bbb88a17a59ff3c47edf55ff08e90c248a91e
krupalsavani <69846583+krupalsavani@users.noreply.github.com>
2020-08-18 23:34:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBZFjdxkLl8AnH_PN8OnQi0ShPw4offqKs",
+  authDomain: "hacking-d54b2.firebaseapp.com",
+  databaseURL: "https://hacking-d54b2.firebaseio.com",
+  projectId: "hacking-d54b2",
+  storageBucket: "hacking-d54b2.appspot.com",
+  messagingSenderId: "1038964485594",
+  appId: "1:1038964485594:web:84fc69b2f42aec5316a4e3",
+  measurementId: "G-ZVBLF577P7"
 };
 
      
